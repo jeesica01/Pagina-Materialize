bcffd279ae92ceba2184d3eb9c1aaab5319936fa
Jessica Solis <jeesica01>
2019-05-13 13:07:38 -0500

    PaginaMaterialize

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+	$(function(){
+		$('.sidenav').sidenav();
+		$('.parallax').parallax();
+		$('#textarea1').val('New Text');
+		M.textareaAutoResize($('#textarea1'));
+	});
+})(jQuery);
